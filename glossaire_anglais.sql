70c85612b9970936932d3f134538c948c1b6f0d2
Rémi <remi.gavin38@gmail.com>
2017-01-25 15:59:20 +0100

    SQL

--- glossaire_anglais.sql
+++ glossaire_anglais.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.5.1
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Mer 25 Janvier 2017 à 15:30
--- Version du serveur :  10.1.19-MariaDB
--- Version de PHP :  5.6.28
+-- Client :  localhost:8889
+-- Généré le :  Mer 25 Janvier 2017 à 15:54
+-- Version du serveur :  5.6.34
+-- Version de PHP :  7.1.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `glossaire_anglais`
@@ -28,24 +22,37 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mot` (
   `num_mot` int(11) NOT NULL,
-  `mot_francais` varchar(255) NOT NULL,
-  `mot_anglais` varchar(255),
-  `num_page` int(11) NOT NULL,
-  PRIMARY KEY `num_mot`,
-  FOREIGN KEY `num_page` REFERENCES `page`.`num_page`
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+  `mot_fr` int(11) NOT NULL,
+  `mot_en` int(11) NOT NULL,
+  `num_page` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Structure de la table `page`
+-- Index pour les tables exportées
 --
 
-CREATE TABLE `page` (
-  `num_page` int(11) NOT NULL,
-  PRIMARY KEY `num_page`,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Index pour la table `mot`
+--
+ALTER TABLE `mot`
+  ADD PRIMARY KEY (`num_mot`),
+  ADD KEY `num_page` (`num_page`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `mot`
+--
+ALTER TABLE `mot`
+  MODIFY `num_mot` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `mot`
+--
+ALTER TABLE `mot`
+  ADD CONSTRAINT `num_page` FOREIGN KEY (`num_page`) REFERENCES `page` (`num_page`);
